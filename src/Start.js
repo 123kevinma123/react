4d9d7860cae426296b65b5b7b761d73e01294eea
123kevinma123 <123kevinma123@gmail.com>
2024-11-01 00:29:00 -0400

    CSS edits

--- src/Start.js
+++ src/Start.js
@@ -1,39 +1,43 @@
-import React, { useEffect, useState} from "react";
-import { BrowserRouter as Router, Route, Routes, Link} from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
-import "./Start.css";
-import Loading from "./Loading.js";
-import Menu from "./Menu.js";
-import MainContent from "./MainContent.js";
-import Footer from "./Footer.js"
-  
-const Button = ( {onClick} ) => {
-    return (
-        <div className = "sub_content">
-            <div className = "button" onClick = {onClick}>
-                Click here!
-            </div>
-        </div>
-    );
+import './Start.css';
+import Loading from './Loading.js';
+import Menu from './Menu.js';
+import MainContent from './MainContent.js';
+import Footer from './Footer.js';
+
+const Button = ({ onClick }) => {
+	return (
+		<div className='sub_content'>
+			<div
+				className='big_button'
+				onClick={onClick}>
+				Click here!
+			</div>
+		</div>
+	);
 };
-  
-const Start = () => {
-    const [isLoading, setIsLoading] = useState(false);
-    const handleButtonClick = () => {
-        setIsLoading(true);
-    }
 
-    return (
-        <div className = "stars">
-            <div className = "rotating_stars" />
-            {isLoading ? <Loading /> : (
-                <>
-                    <MainContent />
-                    <Button onClick = {handleButtonClick} />
-                </>
-            )}
-        </div>
-    );
-  };
-  
-  export default Start;
+const Start = () => {
+	const [isLoading, setIsLoading] = useState(false);
+	const handleButtonClick = () => {
+		setIsLoading(true);
+	};
+
+	return (
+		<div className='stars'>
+			<div className='rotating_stars' />
+			{isLoading ? (
+				<Loading />
+			) : (
+				<>
+					<MainContent />
+					<Button onClick={handleButtonClick} />
+				</>
+			)}
+		</div>
+	);
+};
+
+export default Start;
